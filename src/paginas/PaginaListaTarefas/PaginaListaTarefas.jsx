4845f5e0807bf3793eefd61b9e8077b047d9799a
ROBERT VINÍCIUS NAZARIO <robert_nazario@estudante.sesisenai.org.br>
2024-10-23 20:15:09 -0300

    adicionando atualizações do site

--- src/paginas/PaginaListaTarefas/PaginaListaTarefas.jsx
+++ src/paginas/PaginaListaTarefas/PaginaListaTarefas.jsx
@@ -2,84 +2,58 @@ import Principal from "../../comum/componentes/Principal/Principal";
 import { useState } from "react";
 import BotaoCustomizado from "../../comum/componentes/BotaoCustomizado/BotaoCustomizado";
 import { FaRegTrashCan } from "react-icons/fa6";
+
 const PaginaListaTarefas = () => {
-    const [descricao, setDescricao] = useState('');
-    const [tarefas, setTarefas] = useState([]);
+  const [descricao, setDescricao] = useState("");
+  const [tarefas, setTarefas] = useState([]);
 
-    const adicionarNaLista = () => {
-        if (descricao && descricao.trim()) {
-            tarefas.push({ descricao, feita: false });
-            setTarefas([...tarefas]);
-        }
-        else {
-            alert('Preencha o campo Descrição');
-        }
-        setDescricao('');
-        document.getElementById("campoDescricao").focus()
-    };
-    const removerDaLista = (index) => {
+  const adicionarNaLista = () => {
+    if (descricao && descricao.trim()) {
+      tarefas.push(descricao);
+      setTarefas([...tarefas]);
+    } else {
+      alert("Preencha o campo Descrição");
+    }
+    setDescricao("");
+    document.getElementById("campoDescricao").focus();
+  };
+  const removerDaLista = (index) => {
+    tarefas.splice(index, 1);
+    setTarefas([...tarefas]);
+  };
 
-        if(confirm('Tem Certeza?')){
-            tarefas.splice(index, 1);
-            setTarefas([...tarefas]);
-        }
-
-    };
-    const marcarComoFeita = (index) => {
-        const tarefaEdicao = tarefas[index];
-        tarefas[index] = {
-            ...tarefaEdicao,
-            feita: !tarefaEdicao.feita
-        };
-
-        setTarefas([...tarefas])
-    };
-
-    return (
-        <Principal titulo={`Lista de Tarefas (${tarefas.length})`} voltarPara={"/"}>
-            <div className="listaTarefas-css">
-                <input id="campoDescricao"
-                    type="text"
-                    value={descricao}
-                    placeholder="Descrição da tarefa"
-                    onChange={(e) => setDescricao(e.target.value)}
-                    onKeyUp={(e) => {
-                        if (e.key === 'Enter') {
-                            adicionarNaLista();
-                        }
-                    }} />
-                <BotaoCustomizado aoClicar={adicionarNaLista}>+</BotaoCustomizado>
-            </div>
-            <ul>
-                {tarefas.map((item, index) => {
-                    return (
-                        <li key={index} className="pagina-lista-tarefa_item">
-                            <input
-                                type="checkbox"
-                                checked={item.feita}
-                                onChange={() => marcarComoFeita(index)} />
-                                <span 
-                                style={{
-
-                                }}>
-
-                                </span>
-                            {item.descricao}
-                            <FaRegTrashCan
-                                color="purple"
-                                onClick={() => removerDaLista(index)} />
-                        </li>
-                    )
-                })}
-            </ul>
-            {tarefas.length === 0 && (
-                <span className="pagina-lista-tarefas_mensagem-vazia">
-                    Não tem tarefas listadas.
-                </span>
-            )
+  return (
+    <Principal titulo={"Lista de Tarefas"} voltarPara={"/"}>
+      <div className="listaTarefas-css">
+        <input
+          id="campoDescricao"
+          type="text"
+          value={descricao}
+          placeholder="Descrição da tarefa"
+          onChange={(e) => setDescricao(e.target.value)}
+          onKeyUp={(e) => {
+            if (e.key === "Enter") {
+              adicionarNaLista();
             }
-        </Principal>
-    );
+          }}
+        />
+        <BotaoCustomizado aoClicar={adicionarNaLista}>+</BotaoCustomizado>
+      </div>
+      <ul>
+        {tarefas.map((item, index) => {
+          return (
+            <li key={index}>
+              {item}
+              <FaRegTrashCan
+                color="purple"
+                onClick={() => removerDaLista(index)}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </Principal>
+  );
 };
 
 export default PaginaListaTarefas;
